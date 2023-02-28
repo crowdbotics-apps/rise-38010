bce57d3c43a04a8643bc316cde2815b2d08a80f5
Crowdbotics <team@crowdbotics.com>
2023-02-28 20:57:01 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,125 +1,209 @@
-import React from "react";
-import { Text, View, StyleSheet, TouchableHighlight, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  TextInput,
+  Switch,
+  ScrollView
+} from "react-native";
 
-const SupportSendFeedbackScreen = () => {
+const AccountSettingsScreen = (params) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [notifications, setNotifications] = useState(false);
+  const [emailNotifications, setEmailNotifications] = useState(false);
+  const [smsNotifications, setSmsNotifications] = useState(false);
+  const [deactivateAccount, setDeactivateAccount] = useState(false);
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.labelText}>
-          Email address
-        </Text>
-        <Input
-          placeholder='Enter'
-        />
-      </View>
-      <View style={styles.Textarea}>
-        <Text style={styles.labelText}>
-          Message
-        </Text>
-        <TextInput style={styles.TextAreaInput}
-          numberOfLines={5}
-          multiline={true}
-          placeholder='Enter'
-          placeholderTextColor='#ddd'
-        />
-      </View>
-
-      <View style={styles.SubmitBtn}>
-        <Button>Submit</Button>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.subContainer}>
+          <Text style={styles.subText}>Subscription</Text>
+          <View style={styles.subPallet}>
+            <View style={styles.planDes}>
+              <Text style={[styles.fnt25, styles.boldText]}>Plan</Text>
+              <Text style={styles.fnt16}>Description</Text>
+            </View>
+            <View style={styles.subPricing}>
+              <Text style={[styles.fnt25, styles.boldText]}>$14.99 </Text>
+              <Text style={styles.fnt16}>per month</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.billingContainer}>
+          <Text style={styles.billingText}>Billing information</Text>
+          <View style={styles.nameInput}>
+            <Text style={styles.inputText}>Full Name</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your full name"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
+          </View>
+          <View style={styles.nameInput}>
+            <Text style={styles.inputText}>Email Address</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Email Address"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+          <View style={styles.nameInput}>
+            <Text style={styles.inputText}>Card number</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Card Number"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={cardNumber}
+              onChangeText={(text) => setCardNumber(text)}
+            />
+          </View>
+        </View>
+        <View style={styles.togglesContainer}>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>Notifications</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={notifications}
+              onValueChange={(value) => setNotifications(value)}
+            />
+          </View>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>Email Notifications</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={emailNotifications}
+              onValueChange={(value) => setEmailNotifications(value)}
+            />
+          </View>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>SMS Notifications</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={smsNotifications}
+              onValueChange={(value) => setSmsNotifications(value)}
+            />
+          </View>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>Deactivate Account</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={deactivateAccount}
+              onValueChange={(value) => setDeactivateAccount(value)}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
     flex: 1,
-    height: "100%",
-    backgroundColor: "white"
+    backgroundColor: "#fff"
   },
-  labelText: {
-    fontSize: 14,
-    fontWeight: "400",
-    marginBottom: 10,
-    marginLeft: 15
+  subContainer: {
+    paddingHorizontal: 20,
+    flex: 0.2,
+    justifyContent: "center"
   },
-  Textarea: {
-    marginTop: 30
-  },
-  TextAreaInput: {
-    height: 160,
-    borderWidth: 1,
-    borderColor: "#C4C4C4",
-    borderRadius: 10,
-    textAlignVertical: "top",
-    padding: 10
-  },
-  SubmitBtn: {
-    marginTop: 100
-  }
-});
-
-export default SupportSendFeedbackScreen;
-
-const Button = (props) => {
-  return (
-    <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
-      <View style={[btnStyles.button, {
-        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
-        height: props.height ? props.height : 49,
-        borderWidth: props.borderWidth ? props.borderWidth : 0,
-        borderColor: props.borderColor ? props.borderColor : "#000000"
-      }]}>
-        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>{props.children}</Text>
-      </View>
-    </TouchableHighlight>
-  );
-};
-
-const btnStyles = StyleSheet.create({
-  button: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10
-  },
-  text: {
+  subText: {
+    fontSize: 16,
     fontWeight: "bold",
-    fontSize: 15
-  }
-});
-
-const Input = (props) => {
-  return (
-    <View>
-      <TextInput
-        style={textStyles.input}
-        placeholder={props.placeholder}
-        value={props.value}
-        onChangeText={(num) => props.setValue(num)}
-        placeholderTextColor='#ddd'
-        editable={props.editable !== false}
-      />
-      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
-    </View>
-  );
-};
-
-const textStyles = StyleSheet.create({
-  input: {
-    backgroundColor: "#fff",
-    height: 53,
-    borderColor: "#C4C4C4",
-    color: "#000",
-    borderRadius: 10,
-    fontSize: 14,
-    borderWidth: 1,
-    paddingHorizontal: 10
+    padding: 2,
+    marginVertical: 12,
+    marginLeft: 20
   },
-  error: {
-    fontSize: 13,
-    color: "#FA060D",
-    paddingTop: 8
+  subPallet: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  planDes: {
+    flex: 0.4,
+    padding: 10,
+    // borderWidth: 1,
+    // borderColor: '#979797',
+    justifyContent: "center",
+    alignItems: "flex-start"
+  },
+  subPricing: {
+    flex: 0.6,
+    padding: 10,
+    // borderWidth: 1,
+    // borderColor: '#979797',
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center"
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  fnt25: {
+    fontSize: 25
+  },
+  fnt16: {
+    fontSize: 16
+  },
+  billingContainer: {
+    flex: 0.5,
+    paddingHorizontal: 20 // borderWidth: 1,
+    // borderColor: '#979797',
+  },
+  billingText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 2,
+    marginVertical: 12,
+    marginLeft: 20
+  },
+  inputText: {
+    fontSize: 16,
+    marginLeft: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    borderRadius: 10,
+    padding: 10,
+    paddingLeft: 20,
+    marginVertical: 10,
+    width: "100%"
+  },
+  togglesContainer: {
+    flex: 0.3,
+    paddingHorizontal: 20
+  },
+  toggle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  toggleText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 2,
+    marginVertical: 12,
+    marginLeft: 20
   }
 });
+export default AccountSettingsScreen;
