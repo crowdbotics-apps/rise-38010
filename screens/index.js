29895899bb37d7cde95b103015d67cf353bd876f
Crowdbotics <team@crowdbotics.com>
2023-02-28 20:57:00 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,26 +1,125 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, View, StyleSheet, TouchableHighlight, TextInput } from "react-native";
 
-const NotificationPreferenceScreen = (params) => {
+const SupportSendFeedbackScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.headingTxt}>Notification Preference Screen</Text>
+      <View>
+        <Text style={styles.labelText}>
+          Email address
+        </Text>
+        <Input
+          placeholder='Enter'
+        />
+      </View>
+      <View style={styles.Textarea}>
+        <Text style={styles.labelText}>
+          Message
+        </Text>
+        <TextInput style={styles.TextAreaInput}
+          numberOfLines={5}
+          multiline={true}
+          placeholder='Enter'
+          placeholderTextColor='#ddd'
+        />
+      </View>
+
+      <View style={styles.SubmitBtn}>
+        <Button>Submit</Button>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    margin: 20,
+    padding: 20,
     flex: 1,
-    justifyContent: "space-around"
+    height: "100%",
+    backgroundColor: "white"
   },
-  headingTxt: {
-    fontSize: 24,
-    fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12
+  labelText: {
+    fontSize: 14,
+    fontWeight: "400",
+    marginBottom: 10,
+    marginLeft: 15
+  },
+  Textarea: {
+    marginTop: 30
+  },
+  TextAreaInput: {
+    height: 160,
+    borderWidth: 1,
+    borderColor: "#C4C4C4",
+    borderRadius: 10,
+    textAlignVertical: "top",
+    padding: 10
+  },
+  SubmitBtn: {
+    marginTop: 100
   }
 });
 
-export default NotificationPreferenceScreen;
+export default SupportSendFeedbackScreen;
+
+const Button = (props) => {
+  return (
+    <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
+      <View style={[btnStyles.button, {
+        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+        height: props.height ? props.height : 49,
+        borderWidth: props.borderWidth ? props.borderWidth : 0,
+        borderColor: props.borderColor ? props.borderColor : "#000000"
+      }]}>
+        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>{props.children}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const btnStyles = StyleSheet.create({
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 15
+  }
+});
+
+const Input = (props) => {
+  return (
+    <View>
+      <TextInput
+        style={textStyles.input}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChangeText={(num) => props.setValue(num)}
+        placeholderTextColor='#ddd'
+        editable={props.editable !== false}
+      />
+      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
+    </View>
+  );
+};
+
+const textStyles = StyleSheet.create({
+  input: {
+    backgroundColor: "#fff",
+    height: 53,
+    borderColor: "#C4C4C4",
+    color: "#000",
+    borderRadius: 10,
+    fontSize: 14,
+    borderWidth: 1,
+    paddingHorizontal: 10
+  },
+  error: {
+    fontSize: 13,
+    color: "#FA060D",
+    paddingTop: 8
+  }
+});
